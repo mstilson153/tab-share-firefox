75e779949adee81b8ec75176df4254e9dac2cb5d
Michael Stilson <mstilson153@gmail.com>
2023-01-04 12:02:31 -0500

    First working iteration.

---
+++ src/popup/index.js
@@ -0,0 +1,10 @@
+qrImg = document.querySelector(".qr-code img")
+generateButton = document.querySelector("#GenerateButton")
+
+generateButton.addEventListener("click",()=>{
+  browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
+    let tab = tabs[0];
+    console.log(tab.url);
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${tab.url}`;
+  }, console.error) 
+})
